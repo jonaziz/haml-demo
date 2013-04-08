24eedbddd6213c8b94d55ef05182592c0d371412
Jon Aziz <jonaziz@gmail.com>
2013-04-08 15:55:17 -0400

    Initial commit of haml demo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HamlResume::Application.config.secret_token = 'f33da1b944d899cd77ed137dfbf08d45dd3e358fdd182eb3a4b59c6ab959cb097e0f01a4564169c6e1946d3417f59d52b9ea816bef27d9d2e8e565d2f40d28c6'
